586cd3b54e0b79072cbb6a66c2ae8146ff0ca228
Tony Tiratay <tony.tiratay@gmail.com>
2015-10-05 23:34:38 +0000

    Finish contact form. save to db and permit fields. First list of messages test

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,27 @@
 class ContactsController < ApplicationController
 
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Votre message a bien été envoyé. Merci !"
+      redirect_to new_contact_path
+    else
+      flash[:error] = "Désolé, une erreur a empêchée l'envoi de votre message."
+      redirect_to new_contact_path
+    end
+  end
+
+  def index
+    @contacts = Contact.all
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
+
 end
