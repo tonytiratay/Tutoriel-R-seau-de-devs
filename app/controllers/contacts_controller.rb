5291adb95d3db10e730c6fc13eab8c2aa243582c
Tony Tiratay <tony.tiratay@gmail.com>
2015-10-06 00:11:08 +0000

    Enhance landing

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
       flash[:success] = "Votre message a bien été envoyé. Merci !"
       redirect_to new_contact_path
     else
-      flash[:error] = "Désolé, une erreur a empêchée l'envoi de votre message."
+      flash[:danger] = "Désolé, une erreur a empêchée l'envoi de votre message."
       redirect_to new_contact_path
     end
   end
